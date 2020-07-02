b361c0b6a13f117ef0307052436dc2f1a1109912
theslimreaper925 <lasha.nuroshvili.1@btu.edu.ge>
2020-07-02 22:52:13 +0400

    added first things to start working

--- application.py
+++ application.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/addpage')
+
+@app.route('/addpage', methods=['GET', 'POST'])
 def yourpage():
-    champions = ['Aatrox', 'Annie', 'Ahri', 'Braum']
+    champions = ['Aatrox', 'Annie', 'Ahri', 'Ashe', 'Braum']
+    if request.method == 'POST':
+        champion = request.form['ChampionSelect']
+        nickname = request.form['Name']
+        image = request.files['Image']
+        imageB = image.read()
+        return champions[int(champion)] + ' ' + nickname
     return render_template('addpost.html', champions=champions)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
